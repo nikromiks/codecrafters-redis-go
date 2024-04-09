6b0f0b41a75acd59c4af8b47800746bccae28e0c
Mikhail T <mi.timofeev@mt.com>
2024-04-09 12:26:46 +0300

    <your_commit_message>

--- app/server.go
+++ app/server.go
@@ -1,24 +1,52 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-
+	"log"
 	"net"
-	"os"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
+	// Listen for incoming connections
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		fmt.Println("Error:", err)
+		return
 	}
-	_, err = l.Accept()
+
+	// Ensure we teardown the server when the program exits
+	defer l.Close()
+
+	fmt.Println("Server is listening on port 6379")
+
+	for {
+		// Block until we receive an incoming connection
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error:", err)
+			continue
+		}
+
+		// Handle client connection
+		handleClient(conn)
+	}
+}
+
+func handleClient(conn net.Conn) {
+	// Ensure we close the connection after we're done
+	defer conn.Close()
+
+	// Read data
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		return
+	}
+
+	log.Println("Received data", string(buf[:n]))
+
+	if bytes.Equal(buf[:n], []byte("*1\r\n$4\r\nping\r\n")) {
+		conn.Write([]byte("+PONG\r\n"))
 	}
 }
